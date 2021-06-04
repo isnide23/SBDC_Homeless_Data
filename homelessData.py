5204e10e0c8c87f0c3745153010c0a00dfaac9a2
Ethan Barker <ethanbarker0@gmail.com>
2021-06-04 16:51:07 -0700

    Update homelessData.py

--- homelessData.py
+++ homelessData.py
@@ -1,21 +1,25 @@
+import csv
+import pandas as pd
+
+# Open the file and start the CSV reader
 file = open("data.csv", "r")
-data = []
-headers = []
-responses = []
-for line in file:
-    data.append(line)
-# Get the headers
-for value in data[0].split(","):
-    headers.append(value)
-# Get the responses
-for response in data[2::2]:
-    response_list = {}
-    count = 0
-    for value in response.split(","):
-        count += 1
-        if count < 77:
-            response_list[headers[count - 1]] = value
-    responses.append(response_list)
-for response in responses:
-    print(response['age_range'])
-# print(responses)
+reader = csv.reader(file)
+
+responses = []  # This is a list of a responses (responses are dictionaries, see below)
+headers = next(reader)  # This is a list that has all of the first row headers
+
+count = 0  # Count keeps track of which row we are on
+for row in reader:  # Iterate through each row after the headers
+    count += 1
+    if count % 2 != 0:  # Skip every other row because it's blank
+        continue
+    response = {}  # Response is a dictionary where the headers and the keys and the values are the responses
+    column = 0
+    for value in row:
+        response[headers[column]] = value
+        column += 1
+    responses.append(response)
+
+# Print a frequecny table
+question_to_view = "ethnicity"
+print(pd.Series([response[question_to_view] for response in responses]).value_counts())
